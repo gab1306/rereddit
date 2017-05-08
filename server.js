f665328c59b5dee6d53adf30f22c30441dd90f7e
gab1306 <gabmimouni@gmail.com>
2017-05-08 19:28:00 +0300

    btn

--- server.js
+++ server.js
@@ -24,3 +24,17 @@ app.all('*', function(req, res) {
 
 //start the server
 app.listen(process.env.PORT || '8080');
+
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static('public'));
+// app.use(express.static('node_modules'));
+//
+// app.listen('8000', function(){
+//  console.log("yallah")
+// });
